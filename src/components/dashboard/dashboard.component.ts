1a7c72a8e46c829a8b643368b9940b5deb738127
Kent Adrian Sato <adriankentsato@gmail.com>
2024-11-23 21:56:07 +0800

    MOD: pure react file; changed back to ts for angular; observable test on react

---
+++ src/components/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { ComponentWrapper } from '../component.wrapper';
+import { BreakpointService } from 'src/services/breakpoint.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent extends ComponentWrapper {
+
+  constructor(
+    private bps: BreakpointService,
+  ) { super(); }
+
+  protected override render(): void {
+    import( './dashboard' ).then(({ Dashboard }) => {
+      this.root?.render( Dashboard({
+        breakpointService: this.bps,
+      }) );
+    });
+  }
+
+}
---
+++ src/components/dashboard/dashboard.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import * as React from 'react';
+import { BreakpointService } from 'src/services/breakpoint.service';
+
+export interface IProps {
+    breakpointService: BreakpointService,
+}
+
+export const Dashboard = ( props: IProps ) => <Component { ...props }></Component>;
+
+const Component: React.FunctionComponent<IProps> = ( props ) => {
+    const [currentBp, setCurrentBp] = useState('');
+
+    const { breakpointService } = props;
+
+    useEffect(() => {
+        const s = breakpointService.current.subscribe(( bp ) => {
+            setCurrentBp( bp );
+        });
+
+        return () => {
+            s.unsubscribe();
+        };
+    });
+
+    return (
+        <div>
+            <h1>Hello World, { currentBp }</h1>
+            {(() => {
+                if ( currentBp === 'xsmall' ) {
+                    return <p>very small quasong 🥐🥐</p>
+                } else {
+                    return <div>{ currentBp } croissant 🥐🥐🥐🥐🥐</div>
+                }
+            })()}
+        </div>
+    );
+};
+
